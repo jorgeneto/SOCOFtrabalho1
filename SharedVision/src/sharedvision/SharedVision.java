ada8b607373f481832844e3f55f34136c838b33b
PC <PC@joao>
2015-03-31 12:29:20 +0100

    adicionei o mapa

--- SharedVision/src/sharedvision/SharedVision.java
+++ SharedVision/src/sharedvision/SharedVision.java
@@ -15,7 +15,9 @@ public class SharedVision {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Mapa mapa = new Mapa();
+
+        System.out.println(mapa);
     }
-    
+
 }
